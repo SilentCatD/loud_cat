070dd2181bdcbdcda05aa6c8f22e3d60ead23fa0
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-02-10 16:15:40 +0700

    basic bot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+client.run(BOT_TOKEN)
