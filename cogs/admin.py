f6f23e7fb9e762c9d3f0a22ca965ac8b629235a0
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-02-10 20:14:26 +0700

    Admin cogs

---
+++ cogs/admin.py
@@ -0,0 +1,54 @@
+import logging
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from discord.ext.commands.errors import ExtensionError
+
+
+class AdminCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.log(msg=f"{self.__class__.__name__} loaded!", level=logging.INFO)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def reload_all_cogs(self, ctx: Context):
+        cogs = list(self.bot.extensions.keys())
+        async with ctx.typing():
+            loaded: list[str] = []
+            for cog in cogs:
+                try:
+                    await self.bot.reload_extension(cog)
+                except ExtensionError:
+                    await ctx.send(f"Error reloading {cog}")
+                    return
+                loaded.append(cog)
+        embed = discord.Embed(title=f'Cogs reloaded: \n {"#".join(loaded)}'.replace("#", "\n").replace("cogs.", "- "))
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def reload_cog(self, ctx: Context, cog_name: str):
+        cogs = self.bot.extensions.keys()
+        resolved_cog_name = f"cogs.{cog_name}"
+        if resolved_cog_name not in cogs:
+            await ctx.send(f"Cog `{cog_name}` not existed")
+            return
+        async with ctx.typing():
+            try:
+                await self.bot.reload_extension(resolved_cog_name)
+            except ExtensionError:
+                await ctx.send(f"Error reloading {cog_name}")
+                return
+        embed = discord.Embed(title=f'Cogs reloaded: \n-{cog_name}')
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    cog = AdminCog(bot)
+    logging.info(f"{cog.__class__.__name__} loading...")
+    await bot.add_cog(cog)
--- main.py
+++ main.py
@@ -1,29 +1,55 @@
+import asyncio
+import json
+import logging
+
 import discord
+from discord.ext.commands import Bot
+
 import os
 from dotenv import load_dotenv
 
+# CRITICAL = 50
+# ERROR = 40
+# WARNING = 30
+# INFO = 20
+# DEBUG = 10
+# NOTSET = 0
+
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+f = open("settings.json")
 
+settings = json.load(f)
+prefix = settings["prefix"]
+log_level = settings["log_level"]
+f.close()
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.presences = True
 
-client = discord.Client(intents=intents)
+discord.utils.setup_logging(level=log_level)
+bot = Bot(command_prefix=prefix, intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    logging.log(
+        msg=f"{bot.user} is ready! "
+            f"Settings: {settings}",
+        level=logging.INFO)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+async def load_cogs():
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(f"cogs.{file[:-3]}")
 
-client.run(BOT_TOKEN)
+
+async def main():
+    await load_cogs()
+    await bot.start(BOT_TOKEN)
+
+
+asyncio.run(main())
